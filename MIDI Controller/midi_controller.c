67f277f542e3202fc55b4c3d103bd3b04b0351ea
Julian Richey <julianrichey@gmail.com>
2020-05-25 18:37:52 -0500

    those were the wrong bits
    the right bits are now gotten

--- MIDI Controller/midi_controller.c
+++ MIDI Controller/midi_controller.c
@@ -4,6 +4,102 @@
 #include "usb.h"
 #include "midi.h"
 
+////////////////////////////////////////////////////////////////////////////////
+//joe code
+//#include <msp430.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+//#include "adc.h"
+//#include "usb.h"
+//#include "midi.h"
+
+#define RX_BUFFER_LENGTH 100
+
+static volatile uint8_t RXbuffer[RX_BUFFER_LENGTH];
+static volatile uint8_t RXbufferIDX;
+
+#define RX_BUFFER_LENGTH1 100
+
+static volatile uint8_t RXbuffer1[RX_BUFFER_LENGTH1];
+static volatile uint8_t RXbufferIDX1;
+
+#define UART_TX_READY       (UCA0IFG & UCTXIFG)
+#define UART_RX_READY       (UCA0IFG & UCRXIFG)
+#define UART_TX_DONE        (UCA0IFG & UCTXCPTIFG)
+#define UART_RESET_TX_DONE  (UCA0IFG &= ~UCTXCPTIFG)
+
+//USCI_A1
+#define UART_TX_READY_1       (UCA1IFG & UCTXIFG)
+#define UART_RX_READY_1       (UCA1IFG & UCRXIFG)
+#define UART_TX_DONE_1        (UCA1IFG & UCTXCPTIFG)
+#define UART_RESET_TX_DONE_1  (UCA1IFG &= ~UCTXCPTIFG
+
+void uartSetup(void)
+{
+    P2SEL1 |= (BIT5 | BIT6); // pin 2.5 as tx
+    P2SEL0 &= ~(BIT5 | BIT6); // pin 2.6 as rx
+
+    UCA1CTLW0 = UCSWRST; // reset
+    UCA1CTLW0 |= UCSSEL__SMCLK;
+
+    UCA1BR0 = 6;
+    UCA1BR1 = 0x00;
+    UCA1MCTLW |= UCOS16 | UCBRF_8;
+    UCA1IE |= UCRXIE;
+    UCA1IE |= UCTXIE;
+    UCA1CTLW0 &= ~UCSWRST;
+}
+
+void uartSender(int value)
+{
+    while (!UART_TX_READY)
+                ;
+    UCA1TXBUF = value;
+
+/*      while(!UART_TX_DONE)        // if I leave these lines commented out
+            ;                       // then we will only send one single instance of value
+        UART_RESET_TX_DONE;        // if I comment it out, we will send a constant stream of value
+        */
+}
+
+void clockSetup(void)
+{
+    CSCTL0_H = CSKEY >> 8;
+    CSCTL1 = DCOFSEL_0;
+    CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;
+    CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;
+    CSCTL0_H = 0x01;
+}
+
+#pragma vector=USCI_A1_VECTOR
+__interrupt
+
+void USCI_A1_ISR(void)
+{
+    switch (__even_in_range(UCA1IV, USCI_UART_UCTXCPTIFG))
+    {
+    case USCI_NONE:
+        break;
+    case USCI_UART_UCRXIFG:
+
+        RXbuffer1[RXbufferIDX1] = UCA1RXBUF;
+        RXbufferIDX1 = (RXbufferIDX1 + 1) % RX_BUFFER_LENGTH1;
+        LPM3_EXIT;
+        break;
+    case USCI_UART_UCTXIFG:
+        break;
+    case USCI_UART_UCSTTIFG:
+        break;
+    case USCI_UART_UCTXCPTIFG:
+        break;
+    }
+}
+//end joe code
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 void ports_init(void)            //sets all ports as input with pull down resistors
 {
     P1DIR = 0;                                // 1- set p1 to input (0)
@@ -38,17 +134,25 @@ void ports_init(void)            //sets all ports as input with pull down resist
 uint8_t prev_values[16];
 
 int main(void)
+
 {
+    //put the above code at the top of midi_controller.c
+    //then in main, first call the ports_init(); function, then call clockSetup();
+    //    then uartSetup();. Then in your while loop just call uartSender(samp)
+    //    to send the sample value to your computer thru the uart cable.
 
     ports_init();
+    clockSetup();
+    uartSetup();
     adc_init();
     __delay_cycles(5000);
 
-
     while(1){
-        uint8_t samp = adc_sample(1234);
-        printf("%hu\n", samp);
-        //printf("%d\n", samp*3627/4095); //converts to mV. what cast??
+        uint16_t samp16 = adc_sample(1234);
+        uint8_t samp = samp16 >> 4;
+        printf("%u\n", samp);
+        //printf("%u\n", samp*3627/4095); //converts to mV. what cast??
+        uartSender(samp);
     }
 
     uint8_t curr = 0;
